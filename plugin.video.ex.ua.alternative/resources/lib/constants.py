fc8b23cfc0339b3d707e463c8542f1c0414ca82d
Roman Miroshnychenko <romanvm@yandex.ru>
2014-11-24 09:29:57 +0200

    Added bing.com search

---
+++ plugin.video.ex.ua.alternative/resources/lib/constants.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Name:        constants
+# Author:      Roman Miroshnychenko aka Roman V. M.
+# Created on:  23.11.2014
+# Licence:     GPL v.3: http://www.gnu.org/copyleft/gpl.html
+
+# Numeric codes for search in categories
+SEARCH_CATEGORIES = {'/ru/video/foreign?r=23775': '2',
+                     '/ru/video/our?r=23775': '70538',
+                     '/ru/video/foreign_series?r=23775': '1988',
+                     '/ru/video/our_series?r=23775': '422546',
+                     '/ru/video/cartoon?r=23775': '1989',
+                     '/ru/video/anime?r=23775': '23786',
+                     '/ru/video/documentary?r=23775': '1987',
+                     '/ru/video/trailer?r=23775': '1990',
+                     '/ru/video/clip?r=23775': '1991',
+                     '/ru/video/concert?r=23775': '70533',
+                     '/ru/video/show?r=23775': '28713',
+                     '/ru/video/training?r=23775': '28714',
+                     '/ru/video/sport?r=23775': '69663',
+                     '/ru/video/short?r=23775': '23785',
+                     '/ru/video/theater?r=23775': '70665',
+                     '/ru/video/sermon?r=23775': '371146',
+                     '/ru/video/commercial?r=23775': '371152',
+                     '/ru/video/mobile?r=23775': '607160',
+                     '/ru/video/artist?r=23775': '7513588',
+                     '/73427589?r=23775': '73427589'}
+# Extensions for supported media files
+MEDIA_EXTENSIONS = 'avi|mkv|ts|m2ts|mp4|m4v|flv|vob|mpg|mpeg|iso|mov|wmv|rar|zip|'
+'AVI|MKV|TS|M2TS|MP4|M4V|FLV|VOB|MPG|MPEG|ISO|MOV|WMV|RAR|ZIP|'
+'mp3|aac|ogg|wav|dts|ac3|flac|'
+'MP3|AAC|OGG|WAV|DTS|AC3|FLAC'
+
+if __name__ == "__main__":
+    pass
