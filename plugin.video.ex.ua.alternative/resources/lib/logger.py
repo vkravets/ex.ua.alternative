e04eb50dc47cab83b99d6fd055d1f386296e3385
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2014-12-08 11:57:31 +0200

    Changed logging to the standard DEBUG level.

--- plugin.video.ex.ua.alternative/resources/lib/logger.py
+++ plugin.video.ex.ua.alternative/resources/lib/logger.py
@@ -4,20 +4,17 @@
 # Created:     20.02.2014
 # Licence:     GPL v.3: http://www.gnu.org/copyleft/gpl.html
 
-import xbmcaddon
-
-_addon = xbmcaddon.Addon()
+import xbmc
 
 
 def log(var_name='', variable=None):
     """
     Debug logger.
     """
-    if _addon.getSetting('debug') == 'true':
-        try:
-            print u'plugin.video.ex.ua.alternative. {0}: {1}'.format(var_name, variable)
-        except UnicodeEncodeError:
-            print 'plugin.video.ex.ua.alternative. {0}: {1}'.format(var_name, variable.encode('utf-8'))
+    try:
+        xbmc.log(u'plugin.video.ex.ua.alternative. {0}: {1}'.format(var_name, variable), xbmc.LOGDEBUG)
+    except UnicodeEncodeError:
+        xbmc.log('plugin.video.ex.ua.alternative. {0}: {1}'.format(var_name, variable.encode('utf-8')), xbmc.LOGDEBUG)
 
 
 if __name__ == '__main__':
