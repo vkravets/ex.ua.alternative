a2210671b367cfa88b6dce91c34fd65c630b7c1a
Roman Miroshnychenko <romanvm@yandex.ru>
2016-07-04 21:30:03 +0300

    Starts writing actions

--- plugin.video.ex.ua.alternative/libs/actions.py
+++ plugin.video.ex.ua.alternative/libs/actions.py
@@ -3,4 +3,16 @@
 # E-mail: romanvm@yandex.ua
 # Licence:     GPL v.3: http://www.gnu.org/copyleft/gpl.html
 
+from simpleplugin import Plugin
 
+plugin = Plugin()
+
+
+def root(params):
+    """
+    Plugin root
+    """
+    pass
+
+
+plugin.actions['root'] = root
