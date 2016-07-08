c736e197354fda572c10d83533f7e80d300ddb36
Roman Miroshnychenko <romanvm@yandex.ru>
2016-07-09 00:43:38 +0300

    Adds commands for clearing search history, cache and cookies

--- plugin.video.ex.ua.alternative3/libs/commands.py
+++ plugin.video.ex.ua.alternative3/libs/commands.py
@@ -5,6 +5,7 @@
 
 import os
 import re
+import sys
 import xbmc
 import xbmcgui
 from xbmcaddon import Addon
@@ -24,9 +25,34 @@ def loc_string(id_):
     return addon.getLocalizedString(id_).encode('utf-8')
 
 
+def clear_history():
+    history = os.path.join(config_dir, '__history__.pcl')
+    if os.path.exists(history) and dialog.yesno(loc_string(32025), loc_string(32047)):
+        os.remove(history)
+        if not os.path.exists(history):
+            dialog.notification(addon_id, loc_string(32048), icon=icon, sound=False)
+
+
 def clear_cache():
     cache = os.path.join(config_dir, '__cache__.pcl')
     if os.path.exists(cache) and dialog.yesno(loc_string(32025), loc_string(32026)):
         os.remove(cache)
         if not os.path.exists(cache):
-            dialog.notification(addon_id, loc_string(32017), icon=icon, sound=False)
+            dialog.notification(addon_id, loc_string(32027), icon=icon, sound=False)
+
+
+def clear_cookies():
+    cookies = os.path.join(config_dir, '__cookies__.pcl')
+    if os.path.exists(cookies) and dialog.yesno(loc_string(32025), loc_string(32049)):
+        os.remove(cookies)
+        if not os.path.exists(cookies):
+            dialog.notification(addon_id, loc_string(32050), icon=icon, sound=False)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'history':
+        clear_history()
+    elif sys.argv[1] == 'cache':
+        clear_cache()
+    elif sys.argv[1] == 'cookies':
+        clear_cookies()
